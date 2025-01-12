c331fac4eaa4d0e6ae50a8d0a1b218a63a88ff76
MankiYusuke <takosumi1271@ymail.ne.jp>
2025-01-12 17:19:12 +0900

    テストを通しました

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -6,5 +6,28 @@
 // バブルソート(引数が不適切であればfalseを返す)
 bool bubble_sort(item* begin, const item* end)
 {
-	return false;
+    if (!begin || !end || begin >= end) {
+        return false;
+    }
+
+    size_t size = end - begin;
+
+    bool swapped;
+    for (size_t i = 0; i < size - 1; i++) {
+        swapped = false;
+        for (size_t j = 0; j < size - i - 1; j++) {
+            if (begin[j].key > begin[j + 1].key) {
+                item temp = begin[j];
+                begin[j] = begin[j + 1];
+                begin[j + 1] = temp;
+                swapped = true;
+            }
+        }
+
+        if (!swapped) {
+            break;
+        }
+    }
+
+    return true;
 }
